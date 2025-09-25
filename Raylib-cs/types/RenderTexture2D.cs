d6c5acd52660fa5f84be3424203115a7e15b98ce
Matorio <209939889+Matorio@users.noreply.github.com>
2025-09-25 16:04:10 -0500

    Autonomous render texture.
    Several overloads for a new  "Draw" method were added to the "RenderTexture2D"

--- Raylib-cs/types/RenderTexture2D.cs
+++ Raylib-cs/types/RenderTexture2D.cs
@@ -1,4 +1,5 @@
 using System.Runtime.InteropServices;
+using System.Numerics;
 
 namespace Raylib_cs;
 
@@ -30,12 +31,80 @@ public struct RenderTexture2D : IValidable
         return Raylib.LoadRenderTexture(width, height);
     }
 
-    public static RenderTexture2D Load(System.Numerics.Vector2 size)
+    public static RenderTexture2D Load(Vector2 size)
     {
         return Raylib.LoadRenderTexture((int)size.X, (int)size.Y);
     }
 
-    public void Unload()
+    public readonly void Draw(int x, int y)
+    {
+        Draw(x, y, Color.White);
+    }
+
+    public readonly void Draw(int x, int y, Color color)
+    {
+        Draw(new Vector2(x, y), color);
+    }
+
+    public readonly void Draw(Vector2 position)
+    {
+        Draw(position, Vector2.Zero, 0.0f, Color.White);
+    }
+
+    public readonly void Draw(Vector2 position, Color color)
+    {
+        Draw(position, Vector2.Zero, 0.0f, color);
+    }
+
+    public readonly void Draw(Vector2 position, Vector2 origin)
+    {
+        Draw(position, origin, 0.0f, Color.White);
+    }
+
+    public readonly void Draw(Vector2 position, Vector2 origin, Color color)
+    {
+        Draw(position, origin, 0.0f, color);
+    }
+
+    public readonly void Draw(Vector2 position, Vector2 origin, float rotation, Color color)
+    {
+        Rectangle source = new Rectangle(0, 0, Texture.Width, -Texture.Height);
+        Rectangle dest = new Rectangle(position, Texture.Width, Texture.Height);
+        Raylib.DrawTexturePro(Texture, source, dest, origin, rotation, color);
+    }
+
+    public readonly void Draw(Rectangle target)
+    {
+        Draw(target, Vector2.Zero, 0.0f, Color.White);
+    }
+
+    public readonly void Draw(Rectangle target, Vector2 origin)
+    {
+        Draw(target, origin, 0.0f, Color.White);
+    }
+
+    public readonly void Draw(Rectangle target, Color color)
+    {
+        Draw(target, Vector2.Zero, 0.0f, color);
+    }
+
+    public readonly void Draw(Rectangle target, Vector2 origin, Color color)
+    {
+        Draw(target, origin, 0.0f, color);
+    }
+
+    public readonly void Draw(Rectangle target, Vector2 origin, float rotation)
+    {
+        Draw(target, origin, rotation, Color.White);
+    }
+
+    public readonly void Draw(Rectangle target, Vector2 origin, float rotation, Color color)
+    {
+        Rectangle source = new Rectangle(0, 0, Texture.Width, -Texture.Height);
+        Raylib.DrawTexturePro(Texture, source, target, origin, rotation, color);
+    }
+
+    public readonly void Unload()
     {
         Raylib.UnloadRenderTexture(this);
     }
